cf18092ba44132148eeec1f7c297ed96ba0bdae8
Tim <95186548+timur-tikhomirov@users.noreply.github.com>
2024-08-17 06:30:09 +0300

    Добавил обработчики get, put task

--- handlers.go
+++ handlers.go
@@ -39,6 +39,12 @@ func TaskHandler(res http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodPost:
 		taskPostHandler(res, req)
+	case http.MethodGet:
+		taskGetHandler(res, req)
+	case http.MethodPut:
+		taskPutHandler(res, req)
+	case http.MethodDelete:
+		taskDeleteHandler(res, req)
 	default:
 		http.Error(res, "Метод не поддерживается", http.StatusMethodNotAllowed)
 	}
@@ -49,7 +55,7 @@ func taskPostHandler(res http.ResponseWriter, req *http.Request) {
 	var task Task
 
 	res.Header().Set("Content-Type", "application/json; charset=UTF-8")
-
+	// Получаем тело запроса
 	err := json.NewDecoder(req.Body).Decode(&task)
 	if err != nil {
 		err := errors.New("Ошибка десериализации JSON")
@@ -73,7 +79,7 @@ func taskPostHandler(res http.ResponseWriter, req *http.Request) {
 		task.Date = time.Now().Format(DateFormat)
 	}
 
-	startDate, err := time.Parse(DateFormat, task.Date)
+	_, err = time.Parse(DateFormat, task.Date)
 	if err != nil {
 		err := errors.New("Некорректный формат даты")
 		ErrorResponse.Error = err.Error()
@@ -82,7 +88,7 @@ func taskPostHandler(res http.ResponseWriter, req *http.Request) {
 	}
 
 	// Если дата меньше сегодняшней, устанавливаем следующую дату по правилу
-	if startDate.Before(time.Now()) {
+	if task.Date < time.Now().Format(DateFormat) {
 		if task.Repeat != "" {
 			nextDate, err := NextDate(time.Now(), task.Date, task.Repeat)
 			if err != nil {
@@ -120,8 +126,143 @@ func taskPostHandler(res http.ResponseWriter, req *http.Request) {
 }
 
 // Обработчик GET для /task
+func taskGetHandler(res http.ResponseWriter, req *http.Request) {
+	var task Task
+	// Получаем  id из запроса
+	id := req.URL.Query().Get("id")
+
+	// Проверяем, что id не пустой
+	if id == "" {
+		err := errors.New("id не указан")
+		ErrorResponse.Error = err.Error()
+		json.NewEncoder(res).Encode(ErrorResponse)
+		return
+	}
+
+	// Запрос в БД
+	query := `SELECT id, date, title, comment, repeat FROM scheduler WHERE id = ?`
+	row := DB.QueryRow(query, id)
+	err := row.Scan(&task.ID, &task.Date, &task.Title, &task.Comment, &task.Repeat)
+
+	if err != nil {
+		err := errors.New("Задача с таким id не найдена")
+		ErrorResponse.Error = err.Error()
+		json.NewEncoder(res).Encode(ErrorResponse)
+		return
+
+	}
+
+	// Возвращаем ответ
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	json.NewEncoder(res).Encode(task)
+}
+
 // Обработчик PUT для /task
+func taskPutHandler(res http.ResponseWriter, req *http.Request) {
+	var task Task
+
+	// Получаем id из запроса
+	//id := req.URL.Query().Get("id")
+
+	// Получаем тело запроса
+	err := json.NewDecoder(req.Body).Decode(&task)
+	if err != nil {
+		err := errors.New("Ошибка десериализации JSON")
+		ErrorResponse.Error = err.Error()
+		json.NewEncoder(res).Encode(ErrorResponse)
+		return
+	}
+
+	defer req.Body.Close()
+
+	// Проверяем, что id в теле не пустой
+	if task.ID == "" {
+		err := errors.New("id в теле не указан")
+		ErrorResponse.Error = err.Error()
+		json.NewEncoder(res).Encode(ErrorResponse)
+		return
+	}
+
+	// Проверяем, что id в query не пустой
+	//if id == "" {
+	//	err := errors.New("id в query не указан")
+	//	ErrorResponse.Error = err.Error()
+	//	json.NewEncoder(res).Encode(ErrorResponse)
+	//	return
+	//}
+
+	// Проверяем обязательное поле Title
+	if task.Title == "" {
+		err := errors.New("Не указан заголовок задачи")
+		ErrorResponse.Error = err.Error()
+		json.NewEncoder(res).Encode(ErrorResponse)
+		return
+	}
+
+	// Проверяем дату
+	if task.Date == "" {
+		task.Date = time.Now().Format(DateFormat)
+	}
+
+	_, err = time.Parse(DateFormat, task.Date)
+	if err != nil {
+		err := errors.New("Некорректный формат даты")
+		ErrorResponse.Error = err.Error()
+		json.NewEncoder(res).Encode(ErrorResponse)
+		return
+	}
+
+	// Если дата меньше сегодняшней, устанавливаем следующую дату по правилу
+	if task.Date < time.Now().Format(DateFormat) {
+		if task.Repeat != "" {
+			nextDate, err := NextDate(time.Now(), task.Date, task.Repeat)
+			if err != nil {
+				err := errors.New("Правило повторения указано в неправильном формате")
+				ErrorResponse.Error = err.Error()
+				json.NewEncoder(res).Encode(ErrorResponse)
+				return
+			}
+			task.Date = nextDate
+		} else {
+			task.Date = time.Now().Format(DateFormat)
+		}
+	}
+
+	// Обновляем задачу в базе
+	query := `UPDATE scheduler SET date=?, title=?, comment=?, repeat=? WHERE id=?`
+	result, err := DB.Exec(query, task.Date, task.Title, task.Comment, task.Repeat, task.ID)
+	if err != nil {
+		err := errors.New("Задача с таким id не найдена") // вот эта вот история не работает, посчитаем измененные ряды
+		ErrorResponse.Error = err.Error()
+		json.NewEncoder(res).Encode(ErrorResponse)
+		return
+	}
+
+	// Считаем измененные строки
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		err := errors.New("Не получилось посчитать измененные строки")
+		ErrorResponse.Error = err.Error()
+		json.NewEncoder(res).Encode(ErrorResponse)
+		return
+	}
+
+	if rowsAffected == 0 {
+		err := errors.New("Задача не изменена")
+		ErrorResponse.Error = err.Error()
+		json.NewEncoder(res).Encode(ErrorResponse)
+		return
+	}
+
+	// Возвращаем ответ
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	json.NewEncoder(res).Encode(nil)
+}
+
 // Обработчик DELETE для /task
+func taskDeleteHandler(res http.ResponseWriter, req *http.Request) {}
 
 // Обработчик GET для /tasks
 func GetTasksHandler(res http.ResponseWriter, req *http.Request) {
